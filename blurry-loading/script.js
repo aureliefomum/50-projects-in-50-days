acc276dbe7e75a1b3617634ae8d8a24312faffc5
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-11-19 17:24:02 -0300

    added scale function for  blur and load times

--- blurry-loading/script.js
+++ blurry-loading/script.js
@@ -11,5 +11,19 @@ function blurring(){
     if(load > 99){
         clearInterval(int)
     }
-    console.log(load)
+    loadText.innerText = `${load}%`
+
+    // set the opacity using the scale function
+    //in the arguments:
+    //load stands for the amount of time 
+    //inmin and inmax at 0 to 100 respectively are  the min and max loading values
+    //outMin and outMax are the opacity values, so since we are going from visible to opaque (fading out) we set them to 1 and 0 respectively
+    loadText.style.opacity = scale(load, 0,100, 1, 0)
+
+    //set the blur max 
+    bg.style.filter = `blur(${scale(load,0,100, 30,0)}px)`
+}
+
+const scale = (number, inMin, inMax, outMin, outMax) => {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
