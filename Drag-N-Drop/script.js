6cd499c0e76e309b0b622fe622a4fc0d4b129c63
aureliefomum <azam_aurelie@yahoo.com>
2022-03-28 20:11:00 -0300

    completed drag and drop functions

--- Drag-N-Drop/script.js
+++ Drag-N-Drop/script.js
@@ -12,20 +12,24 @@ for (const empty of empties) {
 }
 
 function dragStart() {
-	console.log("drag start");
+	this.className += " hold";
+	setTimeout(() => (this.className = "invisible"), 0);
 }
 function dragEnd() {
 	console.log("drag end");
 }
-function dragOver() {
+function dragOver(e) {
+	e.preventDefault();
 	console.log("drag over");
 }
-function dragEnter() {
-	console.log("drag enter");
+function dragEnter(e) {
+	e.preventDefault();
+	this.className += " hovered";
 }
 function dragLeave() {
-	console.log("drag leave");
+	this.className = "empty";
 }
 function dragDrop() {
-	console.log("drag drop");
+	this.className = "empty";
+	this.append(fill);
 }
