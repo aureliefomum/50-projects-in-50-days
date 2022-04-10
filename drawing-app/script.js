54d4fb0240979fec61852e2cfa68dfaf3869c69b
aureliefomum <azam_aurelie@yahoo.com>
2022-04-10 17:10:57 -0300

    finished adding mouse event listeners

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -16,6 +16,29 @@ canvas.addEventListener("mousedown", (e) => {
 	isPressed = true;
 	x = e.offsetX;
 	y = e.offsetY;
+
+	console.log(isPressed, x, y);
+});
+
+canvas.addEventListener("mouseup", (e) => {
+	isPressed = false;
+	x = undefined;
+	y = undefined;
+
+	console.log(isPressed, x, y);
+});
+
+canvas.addEventListener("mousemove", (e) => {
+	if (isPressed) {
+		const x2 = e.offsetX;
+		const y2 = e.offsetY;
+
+		drawCircle(x2, y2);
+		drawLine(x, y, x2, y2);
+
+		x = x2;
+		y = y2;
+	}
 });
 
 function drawCircle(x, y) {
@@ -30,9 +53,9 @@ function drawLine(x1, y1, x2, y2) {
 	ctx.moveTo(x1, y1);
 	ctx.lineTo(x2, y2);
 	ctx.strokeStyle = color;
-	ctx.lineWidth = size;
+	ctx.lineWidth = size * 2;
 	ctx.stroke();
 }
 
-drawCircle(100, 200);
-drawLine(300, 300, 300, 500);
+// drawCircle(100, 200);
+// drawLine(300, 300, 300, 500);
