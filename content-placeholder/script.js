c9888907b36f3e5224f0f8b5c2782be5b9a19be4
aureliefomum <azam_aurelie@yahoo.com>
2022-06-12 18:07:38 -0300

    added setTimeout to getData function

--- content-placeholder/script.js
+++ content-placeholder/script.js
@@ -8,6 +8,8 @@ const date = document.getElementById('date')
 const animated_bgs = document.querySelectorAll('.animated-bg')
 const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
 
+setTimeout(getData, 2500)
+
 function getData(){
     header.innerHTML ='<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"> alt=""  />'
     title.innerHTML = 'Lorem ipsum dolor sit amet.'
