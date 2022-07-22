94948d1f72a3433867bf55982514e62f6c54da61
aureliefomum <azam_aurelie@yahoo.com>
2022-07-21 21:17:22 -0300

    loveMe event listener created

---
+++ double-heart-click/script.js
@@ -0,0 +1,17 @@
+const loveMe = document.querySelector('.loveMe')
+const times = document.querySelector('#times')
+
+loveMe.addEventListener('click', (e) => {
+    if(clickTime === 0){
+        clickTime = new Date().getTime()
+    } else {
+        if((new Date().getTime()-clickTime) < 800){
+            createHeart(e)
+            clickTime = 0
+        } else{
+            clickTime = new Date().getTime()
+        }
+    }
+})
+
+
